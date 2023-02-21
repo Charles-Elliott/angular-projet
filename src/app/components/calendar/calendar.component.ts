13687e8713f08b501857707d492b4ee05bc16790
Charles-Elliott <60784428+Charles-Elliott@users.noreply.github.com>
2023-02-21 10:13:35 +0100

    First commit!

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Event} from "../../models/event";
+import {EventsService} from "../../services/events.service";
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit{
 
+  events?: Event[];
+  constructor(private service: EventsService) {  }
+
+  ngOnInit(): void{
+    this.service
+      .fetchAllProducts()
+      .subscribe(e => this.events = e);
+  };
 }
--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Event} from "../models/event";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchProduct (id: number): Observable<Event> {
+    return this.http.get<Event>(`'http://localhost:3000/events/'${id}`);
+  }
+
+  fetchAllProducts (): Observable<Event[]> {
+    return this.http.get<Event[]>('http://localhost:3000/events');
+  }
+
+
 }
